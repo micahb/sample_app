c921fe31a6fbd0073b722bc8ce49d2f56eee12fc
Micah Baldwin <micahb@users.noreply.github.com>
2026-07-21 20:41:23 -0500

    security: rotate exposed secret_token
    The previous secret_token was committed to a public repository.
    This commit rotates it to a new secure random value.
    
    NOTE: The old token should be considered compromised.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'f1902c1d0b5d98c564c486605986fe5c4e38d185d912d99f4995aafea39d3e6e59268d167fc60e0469ac7cc9f0d1ca312ace47ee050637f1b02d6f1c34d6fe6a'
+SampleApp::Application.config.secret_token = '11012d476aee53eb2c2c8357e2716c8420b55a8f64d6b5c6e6a77e0197c1638f62895114958da82381bd24d1544948aaa0429413adf9f4d8562c5d83f95b5e00'
